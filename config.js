4d1eb41becc02b68e369a86350ef7472c7fcec7a
mrbubaluu <81476286+mrbubaluu@users.noreply.github.com>
2022-04-04 18:55:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,29 +11,29 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Yago',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingMorning: 'Buenos dias!',
+	greetingAfternoon: 'Buenas,',
+	greetingEvening: 'Buenas Tardes,',
+	greetingNight: 'De vuelta en la Nochesfera!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '9cddfb34b24067566ccdfd9a59d01d2f', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+	language: 'es', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '39.428440,
+	defaultLongitude: '-0.418830',
 
 	// Autochange
 	autoChangeTheme: true,
